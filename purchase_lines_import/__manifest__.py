f9e90b70b65be72683d323d948ee53439de66a28
jemankhalifa <khalifaeman4@gmail.com>
2021-01-01 17:40:45 +0300

    descriptions of modules

--- purchase_lines_import/__manifest__.py
+++ purchase_lines_import/__manifest__.py
@@ -3,6 +3,8 @@
 { 
     "name": "Purchase Lines Import",
     "summary": """Custom Feature to allow import purchase order lines from excel files""",
+    "Description": """ Allow you to import long list of purchases items from excel file 
+        and create a PO by selecting a vendor, date and all products lines imported""",
     "version": '12.0',   
     "category": 'Purchase',   
     "author": "Eman Khalifa",
